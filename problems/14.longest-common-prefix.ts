02a075a01a13bd8f97075be9b9d9eab1af891f0d
Vinicius Lopes <vinicius.lopes.230996@gmail.com>
2025-01-30 20:04:29 -0300

    implement longest common prefix function

--- problems/14.longest-common-prefix.ts
+++ problems/14.longest-common-prefix.ts
@@ -16,5 +16,16 @@
  * // Explanation: There is no common prefix among the input strings.
  */
 function longestCommonPrefix(strs: string[]): string {
-    return ''
+    let prefix = strs[0]
+
+    for (let i = 1; i < strs.length; i++) {
+        while(strs[i].indexOf(prefix) !== 0) {
+            prefix = prefix.substring(0, prefix.length - 1)
+            if (prefix === "") return ""
+        }
+    }
+
+    return prefix
 }
+
+console.log(["flower","flow","flight"])
